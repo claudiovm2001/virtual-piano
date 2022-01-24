733a0c705c1c01f69e5057693be0ab361b9838e6
Cláudio Vitor Mendonça <57025570+claudiovm2001@users.noreply.github.com>
2022-01-24 18:01:39 -0300

    Minor comment changes

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#include <windows.h> // substitui "stdlib" (mmsystem é incompatível com essa).
-#include <mmsystem.h> // inclui comando para reprodução de arquivos de áudio nos formatos mais comuns.
+#include <windows.h> // substitui "stdlib" (mmsystem Ã© incompatÃ­vel com essa).
+#include <mmsystem.h> // inclui comando para reproduÃ§Ã£o de arquivos de Ã¡udio nos formatos mais comuns.
 #include <locale.h>
 
 int main()
@@ -11,24 +11,24 @@ int main()
     {
         system ("cls");
 
-        printf ("\n  || Instrumentos Musicais 1.0, por Cláudio ||\n\n  Selecione um instrumento:\n\n");
-        printf ("  1-Violão\n  2-Piano\n  3-Trompete\n  4-Violino\n  ");
+        printf ("\n  || Instrumentos Musicais 1.0, por ClÃ¡udio ||\n\n  Selecione um instrumento:\n\n");
+        printf ("  1-ViolÃ£o\n  2-Piano\n  3-Trompete\n  4-Violino\n  ");
 
         char nota = 0, instrumento = 0;
         instrumento = getch (stdin); system ("cls");
 
         if (instrumento == '1' || instrumento == '2' || instrumento == '3' || instrumento == '4')
         {
-            switch (instrumento)
+            switch (instrumento) //faz a seleÃ§Ã£o dos arquivos do instrumento escolhido
             {
                  case '1':
-                 printf ("||Instrumento atual: Violão||\n\n  Pressione as teclas de 1 a 7 para tocar.\n\n >Para selecionar outro instrumento, pressione 0<.\n");
+                 printf ("||Instrumento atual: ViolÃ£o||\n\n  Pressione as teclas de 1 a 7 para tocar.\n\n >Para selecionar outro instrumento, pressione 0<.\n");
 
-                 for (int loop2 = 1; loop2 == 1;)
+                 for (int loop2 = 1; loop2 == 1;) //mantÃ©m o programa rodando o instrumento atual atÃ© que haja cancelamento
                      {nota = getch (stdin);
                       switch (nota)
                       {
-                           case '1': PlaySound (TEXT("guitar_c.wav"), NULL, SND_ASYNC); break; // PlaySound lê e reproduz áudio.
+                           case '1': PlaySound (TEXT("guitar_c.wav"), NULL, SND_ASYNC); break; // PlaySound lÃª e reproduz Ã¡udio.
                            case '2': PlaySound (TEXT("guitar_d.wav"), NULL, SND_ASYNC); break;
                            case '3': PlaySound (TEXT("guitar_e.wav"), NULL, SND_ASYNC); break;
                            case '4': PlaySound (TEXT("guitar_f.wav"), NULL, SND_ASYNC); break;
@@ -47,7 +47,7 @@ int main()
                      {nota = getch (stdin);
                       switch (nota)
                       {
-                           case '1': PlaySound (TEXT("piano_c.wav"), NULL, SND_ASYNC); break; // PlaySound lê e reproduz áudio.
+                           case '1': PlaySound (TEXT("piano_c.wav"), NULL, SND_ASYNC); break; // PlaySound lÃª e reproduz Ã¡udio.
                            case '2': PlaySound (TEXT("piano_d.wav"), NULL, SND_ASYNC); break;
                            case '3': PlaySound (TEXT("piano_e.wav"), NULL, SND_ASYNC); break;
                            case '4': PlaySound (TEXT("piano_f.wav"), NULL, SND_ASYNC); break;
@@ -66,7 +66,7 @@ int main()
                      {nota = getch (stdin);
                       switch (nota)
                       {
-                           case '1': PlaySound (TEXT("trumpet_c.wav"), NULL, SND_ASYNC); break; // PlaySound lê e reproduz áudio.
+                           case '1': PlaySound (TEXT("trumpet_c.wav"), NULL, SND_ASYNC); break; // PlaySound lÃª e reproduz Ã¡udio.
                            case '2': PlaySound (TEXT("trumpet_d.wav"), NULL, SND_ASYNC); break;
                            case '3': PlaySound (TEXT("trumpet_e.wav"), NULL, SND_ASYNC); break;
                            case '4': PlaySound (TEXT("trumpet_f.wav"), NULL, SND_ASYNC); break;
@@ -85,7 +85,7 @@ int main()
                      {nota = getch (stdin);
                       switch (nota)
                       {
-                           case '1': PlaySound (TEXT("violin_c.wav"), NULL, SND_ASYNC); break; // PlaySound lê e reproduz áudio.
+                           case '1': PlaySound (TEXT("violin_c.wav"), NULL, SND_ASYNC); break; // PlaySound lÃª e reproduz Ã¡udio.
                            case '2': PlaySound (TEXT("violin_d.wav"), NULL, SND_ASYNC); break;
                            case '3': PlaySound (TEXT("violin_e.wav"), NULL, SND_ASYNC); break;
                            case '4': PlaySound (TEXT("violin_f.wav"), NULL, SND_ASYNC); break;
@@ -100,7 +100,7 @@ int main()
         }
         else
         {
-            printf ("Comando inválido.\n\n"); system ("pause");
+            printf ("Comando invÃ¡lido.\n\n"); system ("pause");
         }
     }
     system("pause");
